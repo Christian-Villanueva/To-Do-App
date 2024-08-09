39c9891300a5d80d7d761019680b6d7d66af104a
Christian-Villanueva <christianaprilkimvillanueva@gmail.com>
2024-08-09 13:46:54 +0800

    Revision

--- src/assets/Components/DeleteConfirmation.jsx
+++ src/assets/Components/DeleteConfirmation.jsx
@@ -5,7 +5,7 @@ const DeleteConfirmation = ({ show, handleClose, handleConfirm, task, deleteAllT
   const renderModalBody = () => {
     if (task) {
       return <><span style={{ color: "rgb(55, 53, 53)"}}>Are you sure you want to <b>DELETE THIS TASK?</b></span>
-      <strong style={{ color: "rgb(42, 40, 40)"}}>{task?.task}</strong></>;
+      <strong style={{ color: "rgb(55, 53, 53)"}}>{task?.task}</strong></>;
     }
     if (deleteAllType === 'pending') {
       return <><span style={{ color: "rgb(55, 53, 53)"}}>Are you sure you want to <b>DELETE ALL PENDING TASK?</b></span></>;
